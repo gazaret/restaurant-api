c91fc69dcfa8d4a01d58495ed76c432c073881bf
n.poluhin <n.poluhin@xsolla.com>
2021-06-26 20:56:21 +0500

    changed env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,11 @@ import { ConfigEnvironment } from './types';
       useFactory: (configService: ConfigService<ConfigEnvironment>) => {
         return {
           type: 'postgres',
-          host: configService.get<string>('QOVERY_DATABASE_MY_DB_HOST'),
-          port: configService.get<number>('QOVERY_DATABASE_MY_DB_PORT'),
-          username: configService.get<string>('QOVERY_DATABASE_MY_DB_USERNAME'),
-          password: configService.get<string>('QOVERY_DATABASE_MY_DB_PASSWORD'),
-          database: configService.get<string>('QOVERY_DATABASE_MY_DB_DATABASE'),
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
           autoLoadEntities: true,
           synchronize: configService.get<string>('NODE_ENV') !== 'production',
         };
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
 export type ConfigEnvironment = {
   JWT_SECRET: string;
   PORT: number;
-  QOVERY_DATABASE_MY_DB_HOST: string;
-  QOVERY_DATABASE_MY_DB_PORT: number;
-  QOVERY_DATABASE_MY_DB_USERNAME: string;
-  QOVERY_DATABASE_MY_DB_PASSWORD: string;
-  QOVERY_DATABASE_MY_DB_DATABASE: string;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_DATABASE: string;
   NODE_ENV: string;
 };
